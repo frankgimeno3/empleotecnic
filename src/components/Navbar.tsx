db19670ddcb0778d497e81e237bca7b9dbc5bec1
Frank Gimeno <frankgimeno3@gmail.com>
2023-06-22 18:38:46 +0200

    navbar updated

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,28 +1,38 @@
 import Link from "next/link";
-// import Image from "next/image";
+import { getServerSession } from "next-auth";
 
-const Navbar: React.FC = () => {
+async function Navbar() {
+  const session = await getServerSession();
+
   return (
     <nav className="fixed top-0 left-0 right-0 z-10 bg-gray-500 bg-opacity-25 backdrop-filter backdrop-blur-lg flex items-center justify-between p-4 md:text-xs xl:text-base ">
-        <Link href="/" passHref>
-          <div className="bg-opacity-25 text-white rounded-full py-2 px-4 border-2 hover:bg-white hover:text-cyan-950  hover:cursor-pointer">
-            E
-          </div>
-        </Link>
-      <div className="flex items-center space-x-4 xl:pr-5 md:pr-1">
-        <Link href="/registro" passHref>
-          <div className="bg-opacity-25 text-white rounded-full py-2 px-4 border-2 hover:bg-white hover:text-cyan-950  hover:cursor-pointer">
-            Registro
-          </div>
-        </Link>
-        <Link href="/iniciarsesion" passHref>
-          <div className="bg-opacity-25 text-white rounded-full py-2 px-4 border-2 hover:bg-white hover:text-cyan-950  hover:cursor-pointer">
-            Iniciar Sesión
-          </div>
-        </Link>
+      <div className="container mx-auto flex justify-between">
+        <div className="bg-opacity-25 text-white rounded-full py-2 px-4 border-2 hover:bg-white hover:text-cyan-950  hover:cursor-pointer">
+          <Link href="/">E</Link>
+        </div>
+
+        <ul className="flex gap-x-2">
+          {session ? (
+            <>
+              <li className="px-3 py-1">
+                <Link href="/dashboard/profile">Perfil</Link>
+              </li>
+            </>
+          ) : (
+            <>
+ 
+ <li className="bg-opacity-25 text-white rounded-full py-2 px-4 border-2 hover:bg-white hover:text-cyan-950  hover:cursor-pointer">
+                <Link href="/">Login</Link>
+              </li>
+              <li className="bg-opacity-25 text-white rounded-full py-2 px-4 border-2 hover:bg-white hover:text-cyan-950  hover:cursor-pointer">
+                <Link href="/register">Register</Link>
+              </li>
+            </>
+          )}
+        </ul>
       </div>
     </nav>
   );
-};
+}
 
 export default Navbar;
