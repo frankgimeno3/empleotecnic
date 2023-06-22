d6eba098b517e12f920e1453196138a24cf03045
Frank Gimeno <frankgimeno3@gmail.com>
2023-06-22 19:06:07 +0200

    cambio el title

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ import { PageProps } from '../../.next/types/app/layout';
 
 export default function Home() {
   return (
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
+      <div className="z-10 w-full mt-20 max-w-5xl items-center justify-between font-mono text-sm lg:flex">
        Page
        </div>
   )
