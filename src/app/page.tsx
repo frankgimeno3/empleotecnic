ecf20c191f5def22f775e09f57c8460733c60e99
Frank Gimeno <frankgimeno3@gmail.com>
2023-06-22 18:23:49 +0200

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+import Image from 'next/image'
+import React from 'react'
+import { PageProps } from '../../.next/types/app/layout';
+
+
+export default function Home() {
+  return (
+      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
+       Page
+       </div>
+  )
+}
