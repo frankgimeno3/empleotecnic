e60ca14078f8b1d6c8d171fa83d696b0cf7dde5d
Frank Gimeno <frankgimeno3@gmail.com>
2023-06-22 18:48:17 +0200

    implemento maquetacion iniciar sesion

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -23,34 +23,48 @@ function Login() {
   };
 
   return (
-    <div className="justify-center h-[calc(100vh-4rem)] flex items-center">
-      <form
-        onSubmit={handleSubmit}
-        className="bg-neutral-950 px-8 py-10 w-3/12"
-      >
-        {error && <div className="bg-red-500 text-white p-2 mb-2">{error}</div>}
-        <h1 className="text-4xl font-bold mb-7">Iniciar Sesión</h1>
+    <div className="relative w-full h-screen">
+      <video
+        className="absolute top-0 left-0 object-cover w-screen h-screen"
+        src="/video/perfiles.mp4"
+        autoPlay
+        loop
+        muted
+      ></video>
+      <div className="absolute inset-0 flex flex-col justify-center items-center text-center">
+        <div className="bg-gray-800 bg-opacity-60 backdrop-filter backdrop-blur-lg w-50vw  pt-10 pb-10 mt-20 ">
+          <div className="flex flex-col items-center  p-24">
+            <form
+              onSubmit={handleSubmit}
+            >
+              {error && (
+                <div className="bg-red-500 text-white p-2 mb-2">{error}</div>
+              )}
+              <h1 className="text-4xl font-bold mb-7 text-white">Iniciar Sesión</h1>
 
-        <label className="text-slate-300">Email:</label>
-        <input
-          type="email"
-          placeholder="Email"
-          className="bg-zinc-800 px-4 py-2 block mb-2 w-full"
-          name="email"
-        />
+              <label className="text-slate-300 mb-4">Email:</label>
+              <input
+                type="email"
+                placeholder="Email"
+                className="bg-zinc-800 px-4 py-2 block mb-2 w-full"
+                name="email"
+              />
 
-        <label className="text-slate-300">Contraseña:</label>
-        <input
-          type="password"
-          placeholder="Password"
-          className="bg-zinc-800 px-4 py-2 block mb-2 w-full"
-          name="password"
-        />
+              <label className="text-slate-300 mb-4">Contraseña:</label>
+              <input
+                type="password"
+                placeholder="Password"
+                className="bg-zinc-800 px-4 py-2 block mb-2 w-full"
+                name="password"
+              />
 
-        <button className="bg-blue-500 text-white px-4 py-2 block w-full mt-4">
-          Signup
-        </button>
-      </form>
+              <button className="bg-blue-500 text-white px-4 py-2 block w-full mt-4">
+                Signup
+              </button>
+            </form>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
